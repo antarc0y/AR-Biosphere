93932907a5101041d14ab71b709edf4ce78eafa6
mac-op <htta@ualberta.ca>
2023-02-09 19:34:34 -0700

    added naviation between 2 scenes
    TODO: change the x in the second screen to a proper image

---
+++ app/Assets/Scripts/Navigation.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Navigation : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void GoToNextScene() 
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); 
+    }
+
+    public void GoToPreviousScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+
+    // if back button (Android navigation) is pressed
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            if (SceneManager.GetActiveScene().buildIndex == 0)
+            {
+                Application.Quit();
+            }
+            else
+            {
+                GoToPreviousScene();
+            }
+        }
+    }
+}
