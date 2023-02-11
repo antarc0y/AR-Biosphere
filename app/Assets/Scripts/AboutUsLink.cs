1baa595e27f6b5edb7c012cd0de7d069f61ef051
Vaibhav <vchugh@ualberta.ca>
2023-02-11 03:12:15 -0700

    UI update and about us hyperlink

---
+++ app/Assets/Scripts/AboutUsLink.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AboutUsLink : MonoBehaviour
+{
+
+    public void OpenAboutUs(){
+        Application.OpenURL("https://www.aquaticbiosphere.ca/opportunity");
+
+    }
+
+
+}
