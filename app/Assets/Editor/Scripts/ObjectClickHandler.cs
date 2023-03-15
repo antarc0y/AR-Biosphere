4764ab5e518b27f4b6776b4bea187446aed77f95
yuihan <juyi@ualberta.ca>
2023-03-15 12:27:31 -0600

    fixed zoom out on click

--- app/Assets/Editor/Scripts/ObjectClickHandler.cs
+++ app/Assets/Editor/Scripts/ObjectClickHandler.cs
@@ -21,13 +21,13 @@ public class ObjectClickHandler : MonoBehaviour
     public void OnMouseDown()
     {
         // Get the position and forward vector of the AR camera
-        Vector3 cameraPosition = Camera.main.transform.position;
+        Vector3 originalPosition = Camera.main.transform.position;
         Vector3 cameraForward = Camera.main.transform.forward;
 
         if (!isZoomedIn)
         {
             // Calculate a new position for the object in front of the camera, a bit closer to the camera
-            Vector3 newPosition = cameraPosition + cameraForward * zoomDistance - cameraForward.normalized * 0.1f; // 0.5 units in front and 0.1 units closer to the camera
+            Vector3 newPosition = originalPosition + cameraForward * zoomDistance - cameraForward.normalized * 0.1f; // 0.5 units in front and 0.1 units closer to the camera
 
             // Shift the object downwards
             newPosition += Vector3.down * 0.1f;
@@ -36,7 +36,7 @@ public class ObjectClickHandler : MonoBehaviour
             spawnedObject.transform.position = newPosition;
 
             // Rotate the object to face the camera
-            Vector3 direction = cameraPosition - spawnedObject.transform.position;
+            Vector3 direction = originalPosition - spawnedObject.transform.position;
             spawnedObject.transform.rotation = Quaternion.LookRotation(direction, Vector3.up);
 
             // Zoom in the camera
@@ -46,24 +46,11 @@ public class ObjectClickHandler : MonoBehaviour
         }
         else
         {
-            // Zoom out the camera
-            Camera.main.fieldOfView = 60f;
+            // Zoom in the camera
+            Camera.main.fieldOfView = -zoomFOV;
 
-            // Calculate a new position for the object in front of the camera, a bit further from the camera
-            Vector3 newPosition = cameraPosition + cameraForward * (zoomDistance + 0.1f); // 0.1 units further away from the camera
-
-            // Shift the object downwards
-            newPosition += Vector3.down * 0.1f;
-
-            // Set the new position of the object
-            spawnedObject.transform.position = newPosition;
-
-            // Rotate the object to face the camera
-            Vector3 direction = cameraPosition - spawnedObject.transform.position;
-            spawnedObject.transform.rotation = Quaternion.LookRotation(direction, Vector3.up);
-
-            // Zoom out the camera
-            Camera.main.fieldOfView = 60f;
+            // Restore the original position of the object
+            spawnedObject.transform.position = originalPosition;
 
             isZoomedIn = false;
         }
