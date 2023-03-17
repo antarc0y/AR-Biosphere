7675427a9feae39acca2f4fa7e1ea1e4848be6ae
yuihan <juyi@ualberta.ca>
2023-03-17 12:19:51 -0600

    attempt 100 at background blurring

--- app/Assets/Editor/Scripts/ObjectClickHandler.cs
+++ app/Assets/Editor/Scripts/ObjectClickHandler.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.UI;
 using DG.Tweening;
 
 public class ObjectClickHandler : MonoBehaviour
@@ -10,49 +11,66 @@ public class ObjectClickHandler : MonoBehaviour
     public float zoomDistance = 1.5f;
     public float zoomDuration = 0.5f;
 
+    // Blur settings
+    public Image blurBackground;
+    public float blurStrength = 0.5f;
+    public float blurSize = 4f;
+
     // Private state
     private bool isZoomedIn = false;
     private Vector3 originalPosition;
     private Quaternion originalRotation;
 
-public void OnMouseDown()
-{
-    if (!isZoomedIn)
+    public void OnMouseDown()
     {
-        originalPosition = spawnedObject.transform.position;
-        originalRotation = spawnedObject.transform.rotation;
+        if (!isZoomedIn)
+        {
+            originalPosition = spawnedObject.transform.position;
+            originalRotation = spawnedObject.transform.rotation;
 
-        // Animate zooming in
-        spawnedObject.transform.SetParent(Camera.main.transform); // set the spawnedObject as a child of the main camera
-        spawnedObject.transform.DOLocalMove(Vector3.forward * zoomDistance, zoomDuration)
-            .SetEase(Ease.InOutQuad)
-            .SetUpdate(true)
-            .OnComplete(() =>
+            // Animate zooming in
+            spawnedObject.transform.SetParent(Camera.main.transform); // set the spawnedObject as a child of the main camera
+            spawnedObject.transform.DOLocalMove(Vector3.forward * zoomDistance, zoomDuration)
+                .SetEase(Ease.InOutQuad)
+                .SetUpdate(true)
+                .OnComplete(() =>
+                {
+                    isZoomedIn = true;
+                });
+
+            spawnedObject.transform.DOLocalRotateQuaternion(Quaternion.Euler(0f, -180f, 0f), zoomDuration)
+                .SetEase(Ease.InOutQuad)
+                .SetUpdate(true);
+
+            // Enable the blur effect
+            if (blurBackground != null)
             {
-                isZoomedIn = true;
-            });
+                blurBackground.material.SetFloat("_BlurStrength", blurStrength);
+                blurBackground.material.SetFloat("_BlurSize", blurSize);
+                blurBackground.gameObject.SetActive(true);
+            }
+        }
+        else
+        {
+            // Animate zooming out
+            spawnedObject.transform.SetParent(null);
+            spawnedObject.transform.DOLocalMove(originalPosition, zoomDuration)
+                .SetEase(Ease.InOutQuad)
+                .SetUpdate(true)
+                .OnComplete(() =>
+                {
+                    isZoomedIn = false;
+                });
 
-        spawnedObject.transform.DOLocalRotateQuaternion(Quaternion.Euler(0f, -180f, 0f), zoomDuration)
-            .SetEase(Ease.InOutQuad)
-            .SetUpdate(true);
+            spawnedObject.transform.DOLocalRotateQuaternion(originalRotation, zoomDuration)
+                .SetEase(Ease.InOutQuad)
+                .SetUpdate(true);
 
-    }
-    else
-    {
-        // Animate zooming out
-        spawnedObject.transform.SetParent(null);
-        spawnedObject.transform.DOLocalMove(originalPosition, zoomDuration)
-            .SetEase(Ease.InOutQuad)
-            .SetUpdate(true)
-            .OnComplete(() =>
+            // Disable the blur effect
+            if (blurBackground != null)
             {
-                isZoomedIn = false;
-            });
-
-        spawnedObject.transform.DOLocalRotateQuaternion(originalRotation, zoomDuration)
-            .SetEase(Ease.InOutQuad)
-            .SetUpdate(true);
+                blurBackground.gameObject.SetActive(false);
+            }
+        }
     }
-}
-
 }
