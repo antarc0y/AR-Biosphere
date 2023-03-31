151c8fce73dafde398f56f57d87347102b58f3c4
hammad@ualberta.ca <mohammadhammad50@gmail.com>
2023-03-30 20:04:07 -0600

    Fancy like animation zoom fix

--- app/Assets/Scripts/ToggleBehavior.cs
+++ app/Assets/Scripts/ToggleBehavior.cs
@@ -6,7 +6,9 @@ public class ToggleBehavior : MonoBehaviour
 {
     public float fadeDuration = 0.5f;
     public float scaleDuration = 0.2f;
-    public float maxScale = 1.2f;
+    public float maxScale;
+    public float originalScale;
+
     private Image heartImage;
     private Toggle toggle;
     private Color targetColor;
@@ -18,6 +20,10 @@ public class ToggleBehavior : MonoBehaviour
         toggle.onValueChanged.AddListener(OnToggleValueChanged);
 
         heartImage = GetComponent<Image>();
+        originalScale = heartImage.transform.localScale.x;
+
+        maxScale = originalScale + 0.2f;
+
         if (heartImage == null)
         {
             Debug.LogError("ToggleBehavior script must be attached to an Image component!");
@@ -34,18 +40,20 @@ public class ToggleBehavior : MonoBehaviour
             if (isOn)
             {
                 targetColor = Color.red;
-                heartImage.transform.DOScale(maxScale, scaleDuration).SetEase(Ease.OutQuad).OnComplete(() =>
-                {
-                    heartImage.transform.DOScale(1.0f, scaleDuration).SetEase(Ease.OutQuad);
-                });
+
             }
             else
             {
                 targetColor = originalColor;
             }
-
+            
+            heartImage.transform.DOScale(maxScale, scaleDuration).SetEase(Ease.OutQuad).OnComplete(() =>
+            {
+                heartImage.transform.DOScale(originalScale, scaleDuration).SetEase(Ease.OutQuad);
+            });
             heartImage.CrossFadeColor(targetColor, fadeDuration, true, true);
         }
     }
+
 
 }
